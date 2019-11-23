bc4bb7565ded728498b760db4e2c315b15b3fc90
Christian Esteves <chan.esteves@gmail.com>
2019-11-24 04:55:52 +0800

    hotfix - correct routes for multiple floors

--- app/Http/Controllers/BuildingsController.php
+++ app/Http/Controllers/BuildingsController.php
@@ -294,7 +294,7 @@ class BuildingsController extends Controller
 					}
 					else if ($from->floor_id != $to->floor_id && ($adj_via == '' || $via == '' || $adj_via == $via) && $from->floor && $to->floor && $adjascent->destination->floor) {
 						$orig_distance = abs($from->floor->altitude - $to->floor->altitude);
-						$adja_distance = abs($adjascent->destination->floor->altitude - $to->floor->altitude);
+						$adja_distance = abs($adjascent->destination->floor->altitude - $from->floor->altitude);
 
 						if ($adja_distance <= $orig_distance)
 							$links[] = new Link(new MNode($adjascent->origin->longitude, $adjascent->origin->latitude, $adjascent->origin->floor_id), 
